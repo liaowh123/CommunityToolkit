2e21585f64e932aa5f571ad97a685efe574e598c
liaowh123 <lwh809252823@outlook.com>
2025-12-24 10:46:17 +0800

    53453

--- ViewModel/ConfigViewModel.cs
+++ ViewModel/ConfigViewModel.cs
@@ -22,7 +22,8 @@ namespace CommunityToolkit示例.ViewModel
         [RelayCommand]
         private void SaveConfig()
         {
-            ;
+            int a= 1;//待定
+            int b= 2;
         }
     }
 
